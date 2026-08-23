072b2f6a7d1140fcd279c9e484796f5eae92d4e0
Giselle McNeill <gisellemcneill@mac.mynetworksettings.com>
2026-08-22 18:26:48 -0700

    Working add contact form on web page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 contacts = {}
@@ -6,6 +6,16 @@ contacts = {}
 @app.route('/')
 def home():
     return render_template('index.html', contacts = contacts)
+
+@app.route("/add", methods = ["POST"])
+def add():
+    contactName = request.form["contact_name"]
+    contactPhone = request.form["contact_phone"]
+    contactEmail = request.form["contact_email"]
+
+    contacts[contactName] = {"phone": contactPhone, "email": contactEmail}
+
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug = True, port = 5001)
